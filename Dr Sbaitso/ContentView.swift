e10e0a0024113f692ea094bf24e55d2078d615cb
zbornak <zbornak@protonmail.com>
2024-02-02 11:19:18 +0000

    Initial Commit

---
+++ Dr Sbaitso/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dr Sbaitso
+//
+//  Created by Mark Strijdom on 02/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dr Sbaitso/Dr_SbaitsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dr_SbaitsoApp.swift
+//  Dr Sbaitso
+//
+//  Created by Mark Strijdom on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dr_SbaitsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

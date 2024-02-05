69aba489dae331bad53b0ad16eab9aa9c85a99b7
zbornak <zbornak@protonmail.com>
2024-02-05 14:20:32 +0000

    Created userInputView

---
+++ Dr Sbaitso/UserInputView.swift
@@ -0,0 +1,18 @@
+//
+//  UserInputView.swift
+//  Dr Sbaitso
+//
+//  Created by Mark Strijdom on 05/02/2024.
+//
+
+import SwiftUI
+
+struct UserInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserInputView()
+}
